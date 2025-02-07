bcd4be8f796efa2ef796372fdcf4df0b7159fb33
nievesfa <nievesfacorro@gmail.com>
2025-02-07 13:22:19 +0100

    text ok

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,32 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
 
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(new File("/Users/nievesfacorro/Documents/CAMPUSDUAL/FULL_STACK/PROJECTS/BACKEND/Classroom/Ejercicio_31_nievesfa/src/main/resources/lorem.txt")));
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (Exception ex2) {
+                ex2.printStackTrace();
+            }
+        }
     }
 }
